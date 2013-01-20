b1bdcb274a06973f7421598c4b32571e7ece00d5
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2013-01-20 20:25:57 +0400

    Toc not copied now

--- compile.sh
+++ compile.sh
@@ -4,10 +4,8 @@ pdflatex all.tex
 pdflatex all.tex
 ./clean.sh
 cp output/all.pdf ..
-cp output/all.toc ..
 
 pdflatex all_small.tex
 pdflatex all_small.tex
 ./clean.sh
 cp output/all_small.pdf ..
-cp output/all_small.toc ..
