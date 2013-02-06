a3313b112e3c1ee232e94f6dbe52991d2bea5fee
Andrew <iiotep9huy@gmail.com>
2013-02-07 01:34:44 +0400

    Misprints fixed. Thanks to Vanya Reshetnikov

--- compile.sh
+++ compile.sh
@@ -2,10 +2,12 @@ cd src
 
 pdflatex all.tex
 pdflatex all.tex
+pdflatex all.tex
 ./clean.sh
 cp output/all.pdf ..
 
-pdflatex all_small.tex
-pdflatex all_small.tex
-./clean.sh
-cp output/all_small.pdf ..
+#pdflatex all_small.tex
+#pdflatex all_small.tex
+#pdflatex all_small.tex
+#./clean.sh
+#cp output/all_small.pdf ..
